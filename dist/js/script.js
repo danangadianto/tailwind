a1afdca803ac7223fcd9fc07e259c83ac9cb6d2b
Danang Adianto <danangadianto1453@gmail.com>
2022-04-13 22:03:46 +0700

    port

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,15 +10,3 @@ window.onscroll = function() {
     }
 }
 
-
-
-
-
-// Hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
-
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
-})
